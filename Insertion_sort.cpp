a69350563483ac48021ef5d5ca518eba9c84d28e
ADITIYABAYUSATRIA <aditiyayodhasatria@gmail.com>
2026-03-13 16:04:35 +0800

    tambah fungsi display

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -46,3 +46,15 @@ void insertionsort(){
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n============================================";
+    cout << "==========Element Array===========" << endl;
+    cout << "\n============================================" << endl;
+
+    for (int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
